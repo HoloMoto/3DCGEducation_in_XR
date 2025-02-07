823b09ac15d655e82ec48ea99b9c1232d4ff7e58
HoloMoto <36409613+HoloMoto@users.noreply.github.com>
2025-02-07 11:54:54 +0900

    add

--- 3DCGEducation_in_XR/Assets/HoloMoto/SceneTraditionManager.cs
+++ 3DCGEducation_in_XR/Assets/HoloMoto/SceneTraditionManager.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class SceneTraditionManager : MonoBehaviour
 {
+  
   public void SceneTradition(int scene)
   {
+      Debug.Log("SceneTradition in"+scene );
     //sceneのシーンへシーン遷移する
     UnityEngine.SceneManagement.SceneManager.LoadScene(scene);
     
+  }
+
+  public void OpenURLtoNotionWebPage(string url)
+  {
+    //URLを開く
+    Application.OpenURL(url);
   }
 }
